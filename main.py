d211f723a21bd7b83a29efc8e1a4fe86529292ba
angel-skripsi <116819684+angel-skripsi@users.noreply.github.com>
2022-10-27 21:32:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def index():
 
 @app.route("/hello/")
 def home():
-    return "Hello, Flask!";
+    return "Hello, Flask!"
 
 @app.route('/<int:number>/')
 def incrementer(number):
-    return "Incremented number is " + str(number+1);
+    return "Incremented number is " + str(number+1)
 
 # @app.route('/<string:name>/')
 # def hello(name):
@@ -24,15 +24,15 @@ def incrementer(number):
 @app.route('/person/')
 def person():
     return jsonify({'name':'Jimit',
-                    'address':'India'});
+                    'address':'India'})
 
 @app.route('/calculate')
 def calculate():
   x = request.args.get('x', type = int)
   y = request.args.get('y', type = int)
-  return str(my_function(x,y));
+  return str(my_function(x,y))
 
 @app.route('/login_test',methods = ['POST'])
 def login():
     user = request.form['nm'];
-    return user;
+    return user
