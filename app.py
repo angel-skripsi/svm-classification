e669fee1a7a9417024b2696dcf6e8b50b20dfe06
angellinaangell <70848645+angellinaangell@users.noreply.github.com>
2022-11-13 16:40:59 +0700

    add app.py

--- app.py
+++ app.py
@@ -6,23 +6,27 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
-app.config['MYSQL_USER'] = 'b539dadf046091'
-app.config['MYSQL_PASSWORD'] = '5b842ab0'
-app.config['MYSQL_DB'] = 'heroku_97dccdc5801db01'
+@app.route("/hello/")
+def hello():
+  return "hello! this is test"
 
-mysql = MySQL(app)
+# app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
+# app.config['MYSQL_USER'] = 'b539dadf046091'
+# app.config['MYSQL_PASSWORD'] = '5b842ab0'
+# app.config['MYSQL_DB'] = 'heroku_97dccdc5801db01'
 
-@app.route("/select_landsat8/")
-def select_landsat8():
-  cursor = mysql.connection.cursor()
-  cursor.execute("SELECT Wilayah, Kecamatan, Tahun, NDVI, SAVI, EVI FROM landsat_8_pelatihan_raw")
-  rv = cursor.fetchall()
-  cursor.close()
-  payload = []
-  content = {}
-  for result in rv:
-    content = {'Wilayah': result[0], 'Kecamatan': result[1], 'Tahun': result[2], 'NDVI': result[3], 'SAVI': result[4], 'EVI': result[5]}
-    payload.append(content)
-    content = {}
-  return jsonify(payload)
+# mysql = MySQL(app)
+
+# @app.route("/select_landsat8/")
+# def select_landsat8():
+#   cursor = mysql.connection.cursor()
+#   cursor.execute("SELECT Wilayah, Kecamatan, Tahun, NDVI, SAVI, EVI FROM landsat_8_pelatihan_raw")
+#   rv = cursor.fetchall()
+#   cursor.close()
+#   payload = []
+#   content = {}
+#   for result in rv:
+#     content = {'Wilayah': result[0], 'Kecamatan': result[1], 'Tahun': result[2], 'NDVI': result[3], 'SAVI': result[4], 'EVI': result[5]}
+#     payload.append(content)
+#     content = {}
+#   return jsonify(payload)
